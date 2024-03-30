169319a5ab86e43a84f5f47c4f6cc3af4f868252
mclandybeloved@gmail.com <mclandybeloved@gmail.com>
2024-03-30 01:52:33 -0400

    Javascrpt code completed

--- Part 3/Part3_Assignment 4/Assigment4_Part 2/main.js
+++ Part 3/Part3_Assignment 4/Assigment4_Part 2/main.js
@@ -1,18 +1,42 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-/* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+  function thumbnailLoop() {
+  var i;
+  for (i = 0; i < 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', "images/pic" + (i+1) + ".jpg");
+  thumbBar.appendChild(newImage);
+}
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+thumbnailLoop();
 
+/* Making the clicked thumbnail the large image */
+
+thumbBar.addEventListener('click', (e) => {
+  if(e.target && e.target.nodeName == "IMG") {
+    displayedImage.src = e.target.src;
+}
+}); // Close event listener
+
+  
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', (e) => {
+  if (e.target.className == 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+}
+
+ else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+}
+}); // Close event listener
